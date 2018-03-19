ae7ea6f8a1f818034f649e2927d5b37e083bd7f7
jibesh-bhattacharjee <jibesh.nitm@gmail.com>
2018-03-19 22:55:27 +0530

    Initial commit

---
+++ app/src/main/java/com/nitm/contextualize/AddArticleActivity.java
@@ -0,0 +1,13 @@
+package com.nitm.contextualize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_add_article);
+    }
+}
